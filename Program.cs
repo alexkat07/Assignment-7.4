6abd6cd11cebc1c0120b66ed6b0ce5efaeeea199
alexk <alexk@DESKTOP-QL7PNT4>
2024-06-27 11:54:09 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Assignment_7._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    public class ParkingSystem
+    {
+        int bigSpace;
+        int midSpace;
+        int smallSpace;
+
+        public ParkingSystem(int big, int medium, int small)
+        {
+            this.bigSpace = big;
+            this.midSpace = medium;
+            this.smallSpace = small;
+        }
+
+        public bool AddCar(int carType)
+        {
+            switch (carType)
+            {
+                case 3:
+                    if (this.smallSpace != 0)
+                    {
+                        this.smallSpace--;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                case 2:
+                    if (this.midSpace != 0)
+                    {
+                        this.midSpace--;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                case 1:
+                    if (this.bigSpace != 0)
+                    {
+                        this.bigSpace--;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                default:
+                    return false;
+            }
+        }
+    }
+}
